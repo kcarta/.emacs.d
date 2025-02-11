9a9e9b8af762881c7f1f4b5507e515a877090ea5
Kristofer Carta <krc@trifork.com>
2025-02-11 16:33:10 +0100

    Simplify and standardise org config

--- config/org-config.el
+++ config/org-config.el
@@ -1,37 +1,28 @@
-(use-package toc-org
-  :ensure t
-  :commands toc-org-enable
-  :init (add-hook 'org-mode-hook 'toc-org-enable))
-
-;(setq org-hide-block-startup t)
-;; Bug? https://lists.gnu.org/archive/html/emacs-orgmode/2024-02/msg00472.html
 (setq org-startup-indented t)
 
-(use-package org-modern
-  :ensure t
-  :init
-  (add-hook 'org-mode-hook #'org-modern-mode)
-  (add-hook 'org-agenda-finalize-hook #'org-modern-agenda))
 (setq
- ;; Edit settings
  org-auto-align-tags nil
- org-tags-column 0
- org-catch-invisible-edits 'show-and-error
- org-special-ctrl-a/e t
- org-insert-heading-respect-content t
-
- ;; Org styling, hide markup etc.
- org-hide-emphasis-markers t
- org-pretty-entities t)
-
-;; Ellipsis styling
-(setq org-ellipsis "...")
-(set-face-attribute 'org-ellipsis nil :inherit 'default :box nil)
- (global-org-modern-mode)
+ org-tags-column 0)
 
 (setq org-agenda-files '("~/notes"))
-(setq org-refile-use-outline-path 'file)
-(setq org-refile-targets
-    '((nil :maxlevel . 1) (org-agenda-files :level . 1)))
+(setq org-archive-location "~/notes/archive")
+
+(setq org-agenda-remove-tags t)
+
+(setq org-agenda-prefix-format
+      '((todo . " %15c: ")))
+
+(setq org-todo-keywords '((sequence "TODO(t)"
+				    "|"
+				    "DONE(!)" "CANCELED(!)")))
+
+(setq org-priority-lowest 4
+      org-priority-default 3
+      org-priority-highest 1)
+
+;(setq org-refile-use-outline-path 'file)
+
+;(setq org-refile-targets
+    ;'((nil :maxlevel . 1) (org-agenda-files :level . 1)))
 
 (provide 'org-config)
