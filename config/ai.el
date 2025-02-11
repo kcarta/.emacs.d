429dd4649272066a869a1d924a24db163ae329f7
Kristofer Carta <krc@trifork.com>
2025-02-11 16:33:43 +0100

    Use Phi model

--- config/ai.el
+++ config/ai.el
@@ -1,12 +1,11 @@
 (use-package gptel :ensure t
   :config
   (setq
-   gptel-model 'mistral-small ;'phi4:latest
+   gptel-model 'phi4:latest
    gptel-backend (gptel-make-ollama "Ollama"
 		     :host "localhost:11434"
 		     :stream t
-		     ;:models '(phi4:latest)))
-		     :models '(mistral-small)))
+		     :models '(phi4:latest)))
   )
 
 (provide 'ai)
